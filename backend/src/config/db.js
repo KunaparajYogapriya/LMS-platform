6654639d3747a02f5c1a4ea533fba256ef5089ed
KunaparajYogapriya <kpriyavarma662@gmail.com>
2026-03-17 23:07:51 +0530

    Allow self-signed certificates for Aiven compatibility

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -12,7 +12,7 @@ const pool = mysql.createPool({
   connectionLimit: 10,
   queueLimit: 0,
   ssl: process.env.DB_SSL === 'true' ? {
-    rejectUnauthorized: true
+    rejectUnauthorized: false
   } : undefined
 });
 
